551e172df094a2390d95da26574b114ce979e0a4
nikiedev <nikiedev@mail.ru>
2018-08-10 02:20:00 +0300

    renamed the application

--- config/web.php
+++ config/web.php
@@ -11,6 +11,7 @@ $config = [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
     ],
+	'name' => 'Блог: События и Заявки',
     'components' => [
         'request' => [
 	        'baseUrl'=> '',
